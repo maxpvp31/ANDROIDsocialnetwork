86eabe4c9887f499c456531cf1b67b159cd35d8e
Alex <alex.iasoni@isen.yncrea.fr>
2020-02-04 17:58:04 +0100

    FireBase

--- app/src/main/java/fr/isen/lopez/socialapplication/PostModel.kt
+++ app/src/main/java/fr/isen/lopez/socialapplication/PostModel.kt
@@ -1,4 +1,65 @@
 package fr.isen.lopez.socialapplication
 
-class PostModel {
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class PostModel{
+
+
+    val database = FirebaseDatabase.getInstance()
+    companion object {
+        private const val TAG = "Activity"
+    }
+
+    var text: String? = ""
+    var img: String? = ""
+    var date : String?=""
+    var comments : ArrayList<Int>? = ArrayList<Int>()
+    var likes : ArrayList<Int>? = ArrayList<Int>()
+
+   fun InitPostModel(date: String?, text: String?, img: String?, comments: ArrayList<Int>?, likes: ArrayList<Int>? ){
+
+        this.text = text
+       this.img = img
+       this.date = date
+       this.comments = comments
+       this.likes = likes
+
+    }
+
+
+    fun writeNewPost( date: String?, text: String?, img : String?) {
+
+        val dataPost = database.getReference("Posts")
+        val newId = dataPost.push().key.toString()
+        val post = InitPostModel(date, text, img, null, null)
+        dataPost.child(newId).setValue(post)
+    }
+
+
+    fun ReadPosts() {
+
+        val myRef = database.getReference("Posts")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for(value in dataSnapshot.children ) {
+
+                    Log.d(PostModel.TAG, "Value is: ${value}")
+                }
+
+
+            }
+            override fun onCancelled(error: DatabaseError) {
+
+                Log.w(PostModel.TAG, "Failed to read value.", error.toException())
+            }
+        })
+
+    }
+
+
 }
--- app/src/main/java/fr/isen/lopez/socialapplication/UserModel.kt
+++ app/src/main/java/fr/isen/lopez/socialapplication/UserModel.kt
@@ -1,4 +1,38 @@
 package fr.isen.lopez.socialapplication
 
+import com.google.firebase.database.FirebaseDatabase
+
 class UserModel {
+    val database = FirebaseDatabase.getInstance()
+    var email : String? = ""
+    var  nom : String? = ""
+    var prenom : String? = ""
+    var ddnaissance :String? = ""
+    var photo: String? = ""
+    var posts : ArrayList<Int>? = ArrayList<Int>()
+
+   fun PostByUser(posts : ArrayList<Int>){
+        this.posts =  posts
+    }
+
+    fun InitUser(email: String?, nom: String?, prenom: String?, ddnaissance: String?, photo: String?){
+
+        this.email = email
+        this.nom = nom
+        this.prenom = prenom
+        this.ddnaissance = ddnaissance
+        this.photo = photo
+
+    }
+
+
+    fun newUser( email: String?, nom: String?, prenom : String?, ddnaissance: String?) {
+
+        val dataPost = database.getReference("Posts")
+        val newId = dataPost.push().key.toString()
+        val post = InitUser(email, nom, prenom, ddnaissance, null)
+        dataPost.child(newId).setValue(post)
+    }
+
+
 }
