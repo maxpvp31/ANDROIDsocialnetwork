c07b61fef72dc302e95ac928987b27a943319fe1
IlaneLopez <ilane.lopez@outlook.fr>
2020-02-05 13:46:46 +0100

    Merge branch 'master' of https://github.com/IlaneLopez/SocialApplication
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/fr/isen/lopez/socialapplication/LoginActivity.kt
+++ app/src/main/java/fr/isen/lopez/socialapplication/LoginActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-
+    private lateinit var mAuth: FirebaseAuth
     val trueId = "admin"
     var truePassword = "123"
     var userPref: SharedPreferences? = null
@@ -20,7 +23,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        /*
         userPref = getSharedPreferences(UserInfos, 0)
         val checkId = userPref?.getString(kPassword, null) ?: ""
         val checkPassword = userPref?.getString( kIdentifier, null) ?: ""
@@ -30,23 +33,50 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this,HomeActivity::class.java)
             startActivity(intent);
         }
+        */
+        mAuth = FirebaseAuth.getInstance();
 
 
     }
 
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(account: FirebaseUser?) {
+        if (account != null) {
+            Toast.makeText(this, "U Signed In successfully", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, HomeActivity::class.java))
+        } else {
+        }
+    }
 
     public fun switchPage(view: View){
         if(canLog(usernameChild.text.toString(), editPassword.text.toString())){
-            val editor = userPref?.edit()
-            editor?.putString(kIdentifier, trueId)
-            editor?.putString(kPassword,truePassword)
-            editor?.apply();
+            mAuth.signInWithEmailAndPassword(usernameChild.text.toString(), editPassword.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Connexion", "Connexion reussi")
+                        val user = mAuth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Connexion", "Email ou mot de passe incorrect", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
 
-            val intent = Intent(this,HomeActivity::class.java)
-            startActivity(intent);
+                }
+
+
 
         }else{
-            Toast.makeText(this,"Identifiant ou Mot de Passe", Toast.LENGTH_LONG).show()
+            Toast.makeText(this,"Identifiant ou Mot de Passe non remplit", Toast.LENGTH_LONG).show()
         }
     }
 
